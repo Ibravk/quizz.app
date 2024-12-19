74092f828628f9adb2d35940b8bcf53b2aebe416
Ibrahim <poyrazibrahim270@gmail.com>
2024-12-19 16:43:23 +0100

    Nouveau

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,20 @@
-import { BrowserRouter as Router,  Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import './App.css'
+import './App.css';
 import Introduce from './Pages/Introduce/Introduce';
 import Quizz from './Pages/Quizz/Quizz';
 
 function App() {
-
-
   return (
     <div className=''>
-      <Router>
+      <Router basename="/quizz.app"> {/* Ajouter le basename */}
         <Routes>
-          <Route path='/' element={<Introduce/>}></Route>
-          <Route path='/quizz/:difficulty/:amount' element={<Quizz/>}></Route>
-
-
-
-
-
-
+          <Route path='/' element={<Introduce />} />
+          <Route path='/quizz/:difficulty/:amount' element={<Quizz />} />
         </Routes>
       </Router>
-  
     </div>
   );
 }
 
-export default App
+export default App;
